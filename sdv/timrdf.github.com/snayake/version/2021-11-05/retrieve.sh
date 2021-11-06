0ce2e6f2a41aab0cd7cf89a0dda6589d1d48d38c
Timothy Lebo <timrdf@gmail.com>
2021-11-06 17:26:56 -0400

    cleanup

--- sdv/timrdf.github.com/snayake/version/2021-11-05/retrieve.sh
+++ sdv/timrdf.github.com/snayake/version/2021-11-05/retrieve.sh
@@ -25,29 +25,23 @@ if [ ! -e /dev/null ]; then # always false; self-documentation for how to use th
 
 elif [ -e $f/git-repos/version/2021-10-28/manual/retrieve.sh -a -e manual/snayake.sdv.webloc \
                                                              -a -e manual/snayake.properties ]; then
-   $f/git-repos/version/2021-10-28/manual/retrieve.sh # We do not need to bootstrap.
+   $f/git-repos/version/2021-10-28/manual/retrieve.sh # Bootstrap is not needed.
 else
-   mkdir -p manual && h='https://raw.githubusercontent.com/timrdf/snayake/main/sdv/timrdf.github.com' # as in 'http' as opposed to 'f'.
-  #parser="$f/file-formats/version/2021-10-30/manual/H0n3y-BadgeR.sh"
-  #if [ ! -e $parser ]; then
-  #   mkdir -p $(dirname $parser)
-  #   echo $h/file-formats/version/2021-10-30/manual/H0n3y-BadgeR.sh
-  #   curl $h/file-formats/version/2021-10-30/manual/H0n3y-BadgeR.sh > $parser
-  #fi
    trigger='git-repos/version/2021-10-28/manual/retrieve.sh'
    for dependency in file-formats/version/2021-10-30/manual/H0n3y-BadgeR.sh \
                      $trigger; do
       if [ ! -e $f/$dependency ]; then
          mkdir -p $(dirname $f/$dependency)
+         h='https://raw.githubusercontent.com/timrdf/snayake/main/sdv/timrdf.github.com' # as in 'http' as opposed to 'f'.
          echo $h/$dependency
          curl $h/$dependency > $f/$dependency && chmod +x $f/$dependency
       fi
    done
 
+   mkdir -p manual
    curl $h/snayake/version/2021-11-05/manual/snayake.sdv.webloc > manual/snayake.sdv.webloc
    curl $h/snayake/version/2021-11-05/manual/snayake.properties > manual/snayake.properties
-  #curl $h/git-repos/version/2021-10-28/manual/retrieve.sh > retrieve.sh && chmod +x retrieve.sh
-   echo -e "#!/bin/bash\n$f/$trigger" > retrieve.sh && chmod +x retrieve.sh
+   echo -e "#!/bin/bash\n$f/$trigger" > retrieve.sh && chmod +x retrieve.sh # < reads ^
 
    h='https://raw.githubusercontent.com/timrdf/snayake/main/sdv/timrdf.github.com/git-repos/version/2021-10-28/manual/retrieve.sh'
    echo "$h"  #      \\         ||           //
@@ -56,7 +50,7 @@ else
    if [ `md5 -q $f/$trigger` == $verified ]; then
       read -p 'Retrieval digest matches, run it? [y/N] ' runit
    else
-      echo && echo
+      echo
       echo    ' * *   * * ***   * * * * * * * *   *** * **** **'
       echo    ' *  **  ** * *   *  **   ***  *  *  ***  **'
       echo    "$f/$trigger is $(md5 -q $f/$trigger) but expected $verified"
